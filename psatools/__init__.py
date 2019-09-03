618397f6dae8ce803aeb778067664bd4f08016b8
windymel <33108610+windcrusader@users.noreply.github.com>
2019-09-03 13:27:20 +1200

    correct import error

--- psatools/__init__.py
+++ psatools/__init__.py
@@ -1,13 +1,12 @@
 """psatools for electrical engineers
 """
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 from .psatools import(
     calci,
     calcpf,
     calcp,
     calcs,
     calcir,
-    calcc,
     calcq,
 )
